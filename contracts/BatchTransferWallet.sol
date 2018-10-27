82a825bef02a065b4f33cda646100fcaea601b16
seki <schokolade0317@gmail.com>
2018-10-27 15:08:16 +0900

    make batchTransferFrom

--- contracts/BatchTransferWallet.sol
+++ contracts/BatchTransferWallet.sol
@@ -12,6 +12,7 @@ contract ERC20BasicInterface {
     function totalSupply() public view returns (uint256);
     function balanceOf(address who) public view returns (uint256);
     function transfer(address to, uint256 value) public returns (bool);
+    function transferFrom(address from, address to, uint256 value) public returns (bool);
     event Transfer(address indexed from, address indexed to, uint256 value);
 
     uint8 public decimals;
@@ -32,12 +33,24 @@ contract BatchTransferWallet is Ownable {
         ERC20BasicInterface token = ERC20BasicInterface(_tokenAddress);
         require(_investors.length == _tokenAmounts.length && _investors.length != 0);
 
-        uint decimalsForCalc = 10 ** uint256(token.decimals());
+        for (uint i = 0; i < _investors.length; i++) {
+            require(_tokenAmounts[i] > 0 && _investors[i] != 0x0);
+            require(token.transfer(_investors[i], _tokenAmounts[i]));
+        }
+    }
+
+    /**
+    * @dev Send token to multiple address
+    * @param _investors The addresses of EOA that can receive token from this contract.
+    * @param _tokenAmounts The values of token are sent from this contract.
+    */
+    function batchTransferFrom(address _tokenAddress, address[] _investors, uint256[] _tokenAmounts) public {
+        ERC20BasicInterface token = ERC20BasicInterface(_tokenAddress);
+        require(_investors.length == _tokenAmounts.length && _investors.length != 0);
 
         for (uint i = 0; i < _investors.length; i++) {
             require(_tokenAmounts[i] > 0 && _investors[i] != 0x0);
-            _tokenAmounts[i] = _tokenAmounts[i].mul(decimalsForCalc);
-            require(token.transfer(_investors[i], _tokenAmounts[i]));
+            require(token.transferFrom(msg.sender,_investors[i], _tokenAmounts[i]));
         }
     }
 
